6f3e182bcf3a05ca19a9d9ae5d1acd689bffbe37
million <quangtrieu2509@gmail.com>
2022-10-12 01:12:48 +0700

    switch to use cloud mongodb

--- src/configs/config.js
+++ src/configs/config.js
@@ -6,8 +6,8 @@ const REFRESH_SECRET_KEY = process.env.REFRESH_SECRET_KEY || "";
 const PORT = process.env.PORT;
 const DB_HOST = process.env.DB_HOST;
 const DB_PORT = process.env.DB_PORT;
-// export const DB_USER = process.env.DB_USER ;
-// export const DB_PWD = process.env.DB_PWD ;
+const DB_USER = process.env.DB_USER ;
+const DB_PWD = process.env.DB_PWD ;
 const DB_NAME = process.env.DB_NAME;
 
-module.exports = {PORT, DB_HOST, DB_PORT, DB_NAME, BCRYPT_SALT, ACCESS_SECRET_KEY, REFRESH_SECRET_KEY};
+module.exports = {PORT, DB_HOST, DB_PORT, DB_USER, DB_PWD, DB_NAME, BCRYPT_SALT, ACCESS_SECRET_KEY, REFRESH_SECRET_KEY};
--- src/configs/db/index.js
+++ src/configs/db/index.js
@@ -1,18 +1,33 @@
 const mongoose = require('mongoose');
-const {DB_HOST, DB_PORT, DB_NAME} = require('../config');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const {DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PWD} = require('../config');
 
-const db_uri = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+const db_url = `mongodb+srv://${DB_USER}:${DB_PWD}@cluster0.j0fbt91.mongodb.net/${DB_NAME}`;
+
+// const db_uri = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
+// async function connect(){
+
+//     try {
+//         await mongoose.connect(db_uri);
+//         console.log('Connect successfully!');
+//     } catch (error) {
+//         console.log('Connect failed!');
+//     }
+
+// }
 
 async function connect(){
 
-    try {
-        await mongoose.connect(db_uri);
+    try{
+        const client = new MongoClient(db_url, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+        await client.connect();
         console.log('Connect successfully!');
-    } catch (error) {
+    }catch(err){
         console.log('Connect failed!');
     }
     
-
 }
+
 
 module.exports = { connect };
